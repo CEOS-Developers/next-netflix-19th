f31d69705a5a213497782e10857b247e320736a6
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-09 13:25:04 +0900

    refactor: layoutReset

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
@@ -7,51 +7,86 @@ export default function Footer(): JSX.Element {
   const router = useRouter();
   const [activeImage, setActiveImage] = useState('');
 
-    useEffect(() => {
-      if (activeImage) { 
-        router.push(`/${activeImage}`);
-      }
-    }, [activeImage]);
+  useEffect(() => {
+    if (activeImage) {
+      router.push(`/${activeImage}`);
+    }
+  }, [activeImage]);
 
-    const handleImageClick = (image: string) => {
-      if (activeImage === image) return;
-      const newActiveImage = activeImage === image ? '' : image;
-      setActiveImage(newActiveImage);
-    };
-    const getImageSrc = (image: string) => {
-      return activeImage === image ? `/${image}-active.svg` : `/${image}.svg`;
-    };
-  
-  
+  const handleImageClick = (image: string) => {
+    if (activeImage === image) return;
+    const newActiveImage = activeImage === image ? '' : image;
+    setActiveImage(newActiveImage);
+  };
+  const getImageSrc = (image: string) => {
+    return activeImage === image ? `/${image}-active.svg` : `/${image}.svg`;
+  };
+
   return (
     <>
-      <div className="flex items-center justify-center h-[31.7px] w-full bg-[#000000] absolute bottom-0">
-        <div className="w-[121px] h-[4.5px] bg-white mt-[13.12px]"></div>
-      </div>
-      <div className="flex items-center justify-between h-[48px] w-full bg-[#121212] px-[26px] absolute bottom-[31.7px]">
-        <div className="flex flex-col items-center justify-between h-[35px]">
-        <Image src={getImageSrc('home')} alt="Home" width={24} height={24} onClick={() => handleImageClick('home')} />
-          <span className="text-white" style={{ fontSize: '8.2px' }}>Home</span>
+      <div className="flex flex-col fix bottom-0">
+        <div className="flex items-center justify-between h-[48px] w-full bg-[#121212] px-[26px] ">
+          <div className="flex flex-col items-center justify-between h-[35px]">
+            <Image
+              src={getImageSrc('home')}
+              alt="Home"
+              width={24}
+              height={24}
+              onClick={() => handleImageClick('home')}
+            />
+            <span className="text-white" style={{ fontSize: '8.2px' }}>
+              Home
+            </span>
+          </div>
+
+          <div className="flex flex-col items-center justify-between h-[35px]">
+            <Image
+              src={getImageSrc('search')}
+              alt="Search"
+              width={24}
+              height={24}
+              onClick={() => handleImageClick('search')}
+            />
+            <span className="text-white" style={{ fontSize: '8.2px' }}>
+              Search
+            </span>
+          </div>
+
+          <div className="flex flex-col items-center justify-between h-[35px]">
+            <Image
+              src="/comingsoon.svg"
+              alt="Coming Soon"
+              width={20}
+              height={20}
+              onClick={() => handleImageClick('comingsoon')}
+            />
+            <span className="text-white" style={{ fontSize: '8.2px' }}>
+              Coming Soon
+            </span>
+          </div>
+
+          <div className="flex flex-col items-center justify-between h-[35px]">
+            <Image
+              src="/download.svg"
+              alt="Download"
+              width={16}
+              height={19}
+              onClick={() => handleImageClick('download')}
+            />
+            <span className="text-white" style={{ fontSize: '8.2px' }}>
+              Download
+            </span>
+          </div>
+
+          <div className="flex flex-col items-center justify-between h-[35px]">
+            <Image src="/more.svg" alt="More" width={21} height={17} onClick={() => handleImageClick('more')} />
+            <span className="text-white" style={{ fontSize: '8.2px' }}>
+              More
+            </span>
+          </div>
         </div>
-        
-        <div className="flex flex-col items-center justify-between h-[35px]">
-        <Image src={getImageSrc('search')} alt="Search" width={24} height={24} onClick={() => handleImageClick('search')} />
-          <span className="text-white" style={{ fontSize: '8.2px' }}>Search</span>
-        </div>
-        
-        <div className="flex flex-col items-center justify-between h-[35px]">
-        <Image src='/comingsoon.svg' alt="Coming Soon" width={20} height={20} onClick={() => handleImageClick('comingsoon')} />
-          <span className="text-white" style={{ fontSize: '8.2px' }}>Coming Soon</span>
-        </div>
-        
-        <div className="flex flex-col items-center justify-between h-[35px]">
-        <Image src='/download.svg' alt="Download" width={16} height={19} onClick={() => handleImageClick('download')} />
-          <span className="text-white" style={{ fontSize: '8.2px' }}>Download</span>
-        </div>
-        
-        <div className="flex flex-col items-center justify-between h-[35px]">
-        <Image src='/more.svg' alt="More" width={21} height={17} onClick={() => handleImageClick('more')} />
-          <span className="text-white" style={{ fontSize: '8.2px' }}>More</span>
+        <div className="flex items-center justify-center h-[31.7px] w-full bg-[#000000]">
+          <div className="w-[121px] h-[4.5px] bg-white mt-[13.12px]"></div>
         </div>
       </div>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+const plugin = require('tailwindcss/plugin');
 
 const config: Config = {
   content: [
@@ -12,11 +13,26 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
       height: {
         'screen-30': 'calc(100vh - 30px)',
+        'screen-80': 'calc(100vh - 79.7px)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }: any) {
+      const newUtilities = {
+        '.scrollbar-hide': {
+          '-ms-overflow-style': 'none', // IE and Edge
+          'scrollbar-width': 'none', // Firefox
+          '&::-webkit-scrollbar': {
+            display: 'none', // Chrome, Safari, Opera
+          },
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
 };
 export default config;
