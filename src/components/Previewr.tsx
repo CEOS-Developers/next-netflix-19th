4c3c19b966b7f3a5ff0fca451b17b250efc15c20
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-09 13:25:18 +0900

    fix: bugFIx

--- src/components/Previewr.tsx
+++ src/components/Previewr.tsx
@@ -2,26 +2,16 @@ import { PreviewrProps } from '@/lib/types';
 import Image from 'next/image';
 
 export default function Previewr({ fetchData }: PreviewrProps) {
-  console.log('Previewer 컴포넌트');
-
   return (
     <>
       <div className="w-full px-4">
         <div className="text-[26px] font-bold text-white">Previews</div>
 
-        <div className="flex overflow-x-auto pt-3 gap-x-2 hide-scrollbar">
+        <div className="flex overflow-x-auto pt-1 gap-x-2 scrollbar-hide">
           {fetchData.results.map((movie, index) => (
             <div key={index} className="inline-block">
-              <div className="w-[102px] h-[102px] relative overflow-hidden rounded-full bg-white">
-                <Image
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  layout="responsive"
-                  width={102}
-                  height={102}
-                  alt={movie.title}
-                  objectFit="cover"
-                  className="rounded-full"
-                />
+              <div className="w-[102px] h-[102px] relative overflow-hidden rounded-full bg-white cursor-pointer">
+                <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} fill alt={movie.title} />
               </div>
             </div>
           ))}
