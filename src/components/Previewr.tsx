bbed2c50d8153e4d530a760bc9dca34ad3c20067
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-09 12:39:01 +0900

    feat: :lipstick: PreviewsComponent

--- src/components/Previewr.tsx
+++ src/components/Previewr.tsx
@@ -6,16 +6,26 @@ export default function Previewr({ fetchData }: PreviewrProps) {
 
   return (
     <>
-      <div className="w-full pl-4">
-        <div className="text-[26px] font-bold text-white">Previewer 하는중</div>
+      <div className="w-full px-4">
+        <div className="text-[26px] font-bold text-white">Previews</div>
 
-        {/* <div className="flex pt-3 overflow-x-hidden">
+        <div className="flex overflow-x-auto pt-3 gap-x-2 hide-scrollbar">
           {fetchData.results.map((movie, index) => (
-            <div key={index} className="w-[102px] h-[102px] rounded-full bg-white">
-              <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={102} height={102} alt="movie" />
+            <div key={index} className="inline-block">
+              <div className="w-[102px] h-[102px] relative overflow-hidden rounded-full bg-white">
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  layout="responsive"
+                  width={102}
+                  height={102}
+                  alt={movie.title}
+                  objectFit="cover"
+                  className="rounded-full"
+                />
+              </div>
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     </>
   );
