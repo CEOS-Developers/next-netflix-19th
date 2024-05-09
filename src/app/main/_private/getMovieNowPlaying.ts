4e23b71ab12bc8e3a8df5e871dabcd36be7c0525
Programming-Seungwan <swsj4480@gmail.com>
2024-05-09 15:19:06 +0900

    Feat : themovieAPI 연결
    1. url path에 영향을 주지 않는 _private 디렉터리를 만들고, 그 아래에 영화 데이터를 받아오는 함수들을 작성
    2. 해당 함수들을 서버 컴포넌트인 page.tsx 컴포넌트에서 async ~ await 문법을 이용해 리스트 렌더링 진행
    3. 해당 posterpath에 접근 오류 나는 것을 next.config.mjs 설정 파일에서 해결해줌

---
+++ src/app/main/_private/getMovieNowPlaying.ts
@@ -0,0 +1,46 @@
+// 현재 상영 중인 영화 데이터를 반환하는 함수.
+export async function getMovieNowPlaying() {
+  const previewMovieResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.THEMOVIE_API_KEY}`,
+    { cache: 'force-cache' }
+  );
+
+  const previewMovieData = await previewMovieResponse.json();
+
+  return previewMovieData.results;
+}
+
+// 인기 있는 영화 데이터를 반환하는 함수
+export async function getMoviePopular() {
+  const popularMovieResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.THEMOVIE_API_KEY}`,
+    { cache: 'force-cache' }
+  );
+
+  const popularMovieData = await popularMovieResponse.json();
+
+  return popularMovieData.results;
+}
+
+// top-rated 된 영화 데이터를 반환하는 함수
+export async function getMovieTopRated() {
+  const topRatedMovieResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.THEMOVIE_API_KEY}`,
+    { cache: 'force-cache' }
+  );
+
+  const topRatedMovieData = await topRatedMovieResponse.json();
+
+  return topRatedMovieData.results;
+}
+
+export async function getMovieUpComing() {
+  const upComingMovieResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.THEMOVIE_API_KEY}`,
+    { cache: 'force-cache' }
+  );
+
+  const upComingMovieData = await upComingMovieResponse.json();
+
+  return upComingMovieData.results;
+}
