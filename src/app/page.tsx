6eb04018065061287409b0398c1062f8d8ac7224
dreamforxou <shsh9600@naver.com>
2024-05-11 01:28:51 +0900

    chotee :배경 더 밝게 함

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const LandingPage = () => {
         position: 'absolute',
         width: '100%',
         height: '100%',
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
         backdropFilter: 'blur(60px)',
       }}>          
         <NetflixButton />
