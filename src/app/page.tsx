78c5cfa88089214e26e5cdf7869bb056b7f5ed0c
dreamforxou <shsh9600@naver.com>
2024-05-10 22:53:14 +0900

    fix : lottie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,22 @@
 'use client';
-
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
-import '@dotlottie/player-component';
+import Lottie from 'react-lottie-player';
+import netflixLanding from './netflixLanding.json'
 
 const LandingPage = () => {
   const router = useRouter();
-  const [fadeEffect, setFadeEffect] = useState('');
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const timer = setTimeout(() => {
-        router.push('/main');  // 메인 페이지로 리디렉션!
-      }, 5000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [router]);
-
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <dotlottie-player
-        src="https://lottie.host/9ff03e8b-41e2-44e6-889f-38028549e162/0iEaJMZOVi.json"
-        background="transparent"
-        speed="1"
-        style={{ width: '300px', height: '300px' }}
-        autoplay
-      />
-    </div>
+    <Lottie
+      loop={false}
+      animationData={netflixLanding}
+      style={{ width: '300px', height: '300px' }}
+      play
+      onComplete={() => router.push('/main')}
+    />
+  </div>
   );
 };
 
