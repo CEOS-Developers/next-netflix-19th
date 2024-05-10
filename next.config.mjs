9aff25f4506c283773bfd1c3f7d0f36034cc44d4
Programming-Seungwan <swsj4480@gmail.com>
2024-05-10 15:20:43 +0900

    Fix : add headers config in next.config.mjs file for production
    1. 배포 환경에서 chrome 브라우저에서의 보안 문제 해결을 위한 nextJS 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ['image.tmdb.org'],
   },
   webpack: (config) => {
     config.module.rules.push({
@@ -20,6 +20,14 @@ const nextConfig = {
         pathname: '/t/p/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block',
+      },
+    ];
   },
 };
 
