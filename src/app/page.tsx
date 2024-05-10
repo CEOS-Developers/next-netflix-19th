d11870b9d71885063093a918791b94fe54d00102
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-05-10 14:08:44 +0900

    feat: MovieList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
+import BigPoster from '@/components/BigPoster';
 import Poster from '@/components/Poster';
 import Previewr from '@/components/Previewr';
 import { fetchDiscoverMovie } from '@/lib/actions';
 
 const apiEndpoints = {
   popularmovies: '/movie/popular',
+  trendingmovies: '/trending/movie/day?language=en-US',
+  topratedmovies: '/tv/top_rated',
+  ontheairmovies: '/tv/on_the_air',
+  nowplayingmovies: '/movie/now_playing',
+  upcomingmovies: '/movie/upcoming',
 };
 
 export default async function Home() {
-  const fetchData = await fetchDiscoverMovie(apiEndpoints.popularmovies);
+  const fetchPopular = await fetchDiscoverMovie(apiEndpoints.popularmovies);
+  const fetchTrend = await fetchDiscoverMovie(apiEndpoints.trendingmovies);
+  const fetchTopRated = await fetchDiscoverMovie(apiEndpoints.topratedmovies);
+  const fetchOnAir = await fetchDiscoverMovie(apiEndpoints.ontheairmovies);
+  const fetchNowPlaying = await fetchDiscoverMovie(apiEndpoints.nowplayingmovies);
+  const fetchUpComing = await fetchDiscoverMovie(apiEndpoints.upcomingmovies);
 
   return (
     <>
       <div className="pb-3">
-        <Previewr fetchData={fetchData} />
-        <Poster title="Popular on Netflix" fetchData={fetchData} />
-        <Poster title="Trending Now" fetchData={fetchData} />
-        <Poster title="Top 10 in Nigeria Today" fetchData={fetchData} />
-        <Poster title="My List" fetchData={fetchData} />
+        <Previewr fetchData={fetchNowPlaying} />
+        <Poster title="Popular on Netflix" fetchData={fetchPopular} />
+        <Poster title="Trending Now" fetchData={fetchTrend} />
+        <Poster title="Top 10 in Nigeria Today" fetchData={fetchTopRated} />
+        <Poster title="Airing Today" fetchData={fetchOnAir} />
+        <Poster title="Now Playing" fetchData={fetchNowPlaying} />
+        <Poster title="Upcoming" fetchData={fetchUpComing} />
+        <BigPoster title="Netflix Originals" fetchData={fetchPopular} />
+        <Poster title="Popular on Netflix" fetchData={fetchPopular} />
+        <Poster title="Trending Now" fetchData={fetchTrend} />
+        <Poster title="Top 10 in Nigeria Today" fetchData={fetchTopRated} />
+        <Poster title="Airing Today" fetchData={fetchOnAir} />
       </div>
     </>
   );
