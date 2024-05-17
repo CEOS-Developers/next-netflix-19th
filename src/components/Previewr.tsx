ff6530a574619325d8c7c6a53319165522e99b99
dreamforxou <shsh9600@naver.com>
2024-05-17 14:44:42 +0900

    feat : 모든 포스터 상세페이지 routing

--- src/components/Previewr.tsx
+++ src/components/Previewr.tsx
@@ -1,5 +1,6 @@
 import { PreviewrProps } from '@/lib/types';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Previewr({ fetchData }: PreviewrProps) {
@@ -11,9 +12,15 @@ export default function Previewr({ fetchData }: PreviewrProps) {
         <div className="flex overflow-x-auto pt-1 gap-x-2 scrollbar-hide">
           {fetchData.results.map((movie, index) => (
             <div key={index} className="inline-block">
+              <Link href={`/detail/${movie.id}`}>
               <div className="w-[102px] h-[102px] relative overflow-hidden rounded-full bg-white cursor-pointer">
-                <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} fill alt={movie.title} />
+                <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                       fill 
+                       alt={movie.title} 
+                       style={{ objectFit: 'cover' }}
+                />
               </div>
+              </Link>
             </div>
           ))}
         </div>
